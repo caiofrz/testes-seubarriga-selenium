c34baef4932cd8279e6bef4237233671603d60f8
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2024-03-20 12:48:32 -0300

    test(Login): Add test that checks for error cases in login

--- src/test/java/com/caio_ikaro/seubarriga/auth/LoginTest.java
+++ src/test/java/com/caio_ikaro/seubarriga/auth/LoginTest.java
@@ -1,12 +1,91 @@
 package com.caio_ikaro.seubarriga.auth;
 
 import com.caio_ikaro.seubarriga.BaseProperties;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
 
+  WebDriver driver;
+
+  @BeforeEach
+  void setup() {
+    driver = new ChromeDriver();
+    driver.get(BaseProperties.BASE_URL + "/login");
+  }
+
   @Test
-  void teste() {
-    System.err.println(BaseProperties.BASE_URL);
+  void deveRetornarErroAoLogarUmUsuarioNaoCadastrado() {
+
+    WebElement email = driver.findElement(By.id("email"));
+    email.sendKeys("teste9999999999999999990@email.com");
+    WebElement senha = driver.findElement(By.id("senha"));
+    senha.sendKeys(BaseProperties.PASSWORD);
+
+    WebElement cadastrar = driver.findElement(By.cssSelector("[type='submit']"));
+    cadastrar.click();
+
+    WebElement error = driver.findElement(By.className("alert-danger"));
+
+    String errorMessageExpected = "Problemas com o login do usuário";
+
+    Assertions.assertEquals(errorMessageExpected, error.getText());
+  }
+
+  @Test
+  void deveRetornarErroAoTentarLogarSemEmail() {
+
+    WebElement email = driver.findElement(By.id("email"));
+    email.sendKeys("");
+    WebElement senha = driver.findElement(By.id("senha"));
+    senha.sendKeys(BaseProperties.PASSWORD);
+
+    WebElement cadastrar = driver.findElement(By.cssSelector("[type='submit']"));
+    cadastrar.click();
+
+    WebElement error = driver.findElement(By.className("alert-danger"));
+
+    String errorMessageExpected = "Email é um campo obrigatório";
+
+    Assertions.assertEquals(errorMessageExpected, error.getText());
+  }
+
+  @Test
+  void deveRetornarErroAoTentarLogarSemSenha() {
+
+    WebElement email = driver.findElement(By.id("email"));
+    email.sendKeys(BaseProperties.EMAIL);
+    WebElement senha = driver.findElement(By.id("senha"));
+    senha.sendKeys("");
+
+    WebElement cadastrar = driver.findElement(By.cssSelector("[type='submit']"));
+    cadastrar.click();
+
+    WebElement error = driver.findElement(By.className("alert-danger"));
+
+    String errorMessageExpected = "Senha é um campo obrigatório";
+
+    Assertions.assertEquals(errorMessageExpected, error.getText());
+  }
+
+  @Test
+  void deveRetornarErroAoTentarLogarComEmailInvalido() throws InterruptedException {
+
+    WebElement email = driver.findElement(By.id("email"));
+    email.sendKeys(BaseProperties.INAVLID_EMAIL);
+    WebElement senha = driver.findElement(By.id("senha"));
+    senha.sendKeys(BaseProperties.PASSWORD);
+
+    WebElement cadastrar = driver.findElement(By.cssSelector("[type='submit']"));
+    cadastrar.click();
+
+    Thread.sleep(2000);
+    //Se fosse um login com sucesso deveria ser redirecionado para /logar
+    Assertions.assertNotEquals(BaseProperties.BASE_URL + "/logar", driver.getCurrentUrl());
   }
 }
