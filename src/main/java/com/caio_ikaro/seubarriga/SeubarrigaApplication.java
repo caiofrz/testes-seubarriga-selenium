616361b7ed5626a949e4b4b44ed9aaa83738fee7
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2024-03-20 11:06:35 -0300

    initial commit

---
+++ src/main/java/com/caio_ikaro/seubarriga/SeubarrigaApplication.java
@@ -0,0 +1,13 @@
+package com.caio_ikaro.seubarriga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeubarrigaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeubarrigaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caio_ikaro/seubarriga/SeubarrigaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caio_ikaro.seubarriga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeubarrigaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
